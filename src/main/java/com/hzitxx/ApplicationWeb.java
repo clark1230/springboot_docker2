9ed18470fd0948e394b7a0abb822620368117467
clark1230 <3267672731@qq.com>
2018-10-27 16:03:36 +0800

    项目初始化!

---
+++ src/main/java/com/hzitxx/ApplicationWeb.java
@@ -0,0 +1,14 @@
+package com.hzitxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 入口类
+ */
+@SpringBootApplication
+public class ApplicationWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationWeb.class,args);
+    }
+}
